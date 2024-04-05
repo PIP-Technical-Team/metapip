bc4d04204bf69517022245eb62fa365f29299d63
shahronak47 <shahronak47@yahoo.in>
2024-04-05 12:55:20 +0800

    update based on feedback

--- R/package_branches.R
+++ R/package_branches.R
@@ -1,9 +1,9 @@
-#' Status tables of package versions in different branches
-#' Apart from PROD and DEV, we are only showing those branches whose version is higher than DEV.
+#' Status tables of package versions in different branches along with local installations.
+#' For local installation,a status column is returned which indicates if the local version is ahead or behind the DEV branch.
 #'
 #' @param package One (or more) of the PIP core packages. Default NULL will include all the packages
 #'
-#' @return tibble of pipr packages and the corresponding package versions of branch
+#' @return tibble of pip packages and the corresponding package versions of branch
 #' @examples
 #' \dontrun{
 #' package_branches()
@@ -28,20 +28,16 @@ package_branches  <- function(package = NULL) {
       mat[, "Version"]
     })
   }, simplify = FALSE)
-  # Keep only package version above DEV version along with master and reshape the data in wider format
+
   complete_data <- all_package_version %>%
     utils::stack() %>%
     tibble::rownames_to_column(var = "branch") %>%
     dplyr::rename(version = .data$values, package = .data$ind) %>%
-    dplyr::mutate(branch = stringr::str_extract(.data$branch, "([0-9A-Za-z-_]+)/DESCRIPTION\\.Version", group = 1)) %>%
-    dplyr::group_by(.data$package) %>%
-    dplyr::filter(.data$branch %in% c("PROD", "DEV") |
-                    purrr::map_lgl(version, ~utils::compareVersion(.x, .data$version[match("DEV", .data$branch)]) == 1)) %>%
-    dplyr::ungroup()
+    dplyr::mutate(branch = stringr::str_extract(.data$branch, "([0-9A-Za-z-_]+)/DESCRIPTION\\.Version", group = 1))
 
     common <- complete_data %>%
       dplyr::filter(.data$branch %in% c("PROD", "DEV", "QA")) %>%
-      tidyr::pivot_wider(names_from = .data$branch, values_from = .data$version) %>%
+      tidyr::pivot_wider(names_from = "branch", values_from = "version") %>%
       dplyr::relocate("package", "PROD")
 
     result <- complete_data %>%
@@ -49,7 +45,23 @@ package_branches  <- function(package = NULL) {
       split(.$package) %>%
       purrr::map(., ~.x %>% dplyr::select(-"package"))
 
-    c(list(common = common), result)
+    # Show local installation
+    local <-  purrr::map_df(package, \(.x) {
+      out <- utils::packageDescription(.x, fields = c("GithubRef", "Version"))
+      tibble::tibble(package = .x, local_branch = out$GithubRef, local_version = out$Version)
+    })
+    # DEV data
+    dev <- complete_data %>% dplyr::filter(.data$branch %in% "DEV")
+    # Join dev data with local data to create status column
+    local <- dplyr::full_join(local, dev, dplyr::join_by(package)) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(status = utils::compareVersion(.data$version, .data$local_version)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(status = dplyr::case_when(status == 1 ~ "behind",
+                                              status == -1 ~ "ahead",
+                                              TRUE ~ "up-to-date")) %>%
+      dplyr::select(-"branch", -"version")
+    c(list(common = common, local = local), result)
 }
 
 
--- tests/testthat/test-package_branches.R
+++ tests/testthat/test-package_branches.R
@@ -1,5 +1,7 @@
 test_that("package_branches works correctly", {
  mockery::stub(package_branches, "get_branches", function(...) c("PROD", "DEV"))
  out <- package_branches(c("pipapi", "wbpip"))
- expect_length(out, 3)
+ expect_length(out, 4)
+ expect_length(out$local, 4)
+ expect_length(out$common, 3)
 })
